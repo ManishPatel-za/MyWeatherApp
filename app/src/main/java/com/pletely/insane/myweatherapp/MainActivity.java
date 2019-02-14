1e58ebab28240cad4a32b5cd48ded235f0bb323f
Manish <Manish.Patel@fnb.co.za>
2019-02-14 17:40:55 +0200

    Changed to Androidx instead of support library, fixed getting of current location, added line between current weather & the forecast recycler.

--- app/src/main/java/com/pletely/insane/myweatherapp/MainActivity.java
+++ app/src/main/java/com/pletely/insane/myweatherapp/MainActivity.java
@@ -1,49 +1,56 @@
 package com.pletely.insane.myweatherapp;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.pletely.insane.myweatherapp.pojos.current.CurrentWeather;
 import com.pletely.insane.myweatherapp.pojos.fiveday.FiveDayWeather;
 import com.pletely.insane.myweatherapp.retrofit.RetrofitClientInstance;
 import com.pletely.insane.myweatherapp.retrofit.WeatherService;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
+    private static final String metric = "metric";
 
-    private RecyclerView forecastRecycler;
-    private TextView currentTemp, minTemp, currentMiniTemp, maxTemp;
-    private ImageView weatherBackground;
+    private RecyclerView mForecastRecycler;
+    private TextView mCurrentTemp, mMinTemp, mCurrentMiniTemp, mMaxTemp;
+    private ImageView mWeatherBackground;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     //TODO: 2. Remove API KEY from code base before commiting!!!
     //TODO: 3. elevation for controls
     //TODO: 4. toolbar & actionbar
     //TODO: 5. unit(metric VS imperial)
-    //TODO: 6. if time:choose sea or forest theme/firebase login/unit test/control animations
-    private static final String API_KEY = "";
+    //TODO: 6. Onsave instance and restore
+    //TODO: 7. Handle for tablet Mode
+    //TODO: 8. if time:choose sea or forest theme/firebase login/unit test/control animations
+    private static final String API_KEY = "760d62673766596fe58627fa0102e7ce";
 
     //Default to Rosebank if no location is found
     //TODO 1. set the lats and longs
-    private double latitude = -26.147133;
-    private double longitude = 28.052434;
+    private double mLatitude = -26.147133;
+    private double mLongitude = 28.052434;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,12 +64,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void initialiseViews() {
 
-        minTemp = (TextView) findViewById(R.id.min_temp);
-        maxTemp = (TextView) findViewById(R.id.max_temp);
-        currentMiniTemp = (TextView) findViewById(R.id.current_mini_temp);
-        currentTemp = (TextView) findViewById(R.id.current_temp);
-        weatherBackground = (ImageView) findViewById(R.id.weather_background);
-        forecastRecycler = (RecyclerView) findViewById(R.id.forecast_recycler);
+        mMinTemp = (TextView) findViewById(R.id.min_temp);
+        mMaxTemp = (TextView) findViewById(R.id.max_temp);
+        mCurrentMiniTemp = (TextView) findViewById(R.id.current_mini_temp);
+        mCurrentTemp = (TextView) findViewById(R.id.current_temp);
+        mWeatherBackground = (ImageView) findViewById(R.id.weather_background);
+        mForecastRecycler = (RecyclerView) findViewById(R.id.forecast_recycler);
     }
 
     private void checkLocationPermission() {
@@ -75,16 +82,28 @@ public class MainActivity extends AppCompatActivity {
                     MY_PERMISSIONS_REQUEST_LOCATION);
         } else {
 
-            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            if (lm != null) {
-                Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                if (location != null) {
-                    longitude = location.getLongitude();
-                    latitude = location.getLatitude();
-                }
-            }
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // GPS location can be null if GPS is switched off
+                            if (location != null) {
+                                mLatitude = location.getLatitude();
+                                mLongitude = location.getLongitude();
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "Error trying to get last GPS location");
+                            e.printStackTrace();
+                        }
+                    });
 
-            fetchWeatherData(String.valueOf(latitude), String.valueOf(longitude), "metric");
+
+            fetchWeatherData(String.valueOf(mLatitude), String.valueOf(mLongitude), metric);
         }
     }
 
